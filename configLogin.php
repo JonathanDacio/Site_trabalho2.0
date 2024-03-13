3f914347bb7c746cd5ec89a7900d10042ff13aa7
Jonathan <dacio.jhow04@gmail.com>
2024-03-13 17:59:36 -0300

    sistema login

--- configLogin.php
+++ configLogin.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 //print_r($_REQUEST); 
 if(isset($_POST['submit']) && !empty($_POST['email'])&& !empty($_POST['senha'])){
 
@@ -10,9 +11,17 @@ if(isset($_POST['submit']) && !empty($_POST['email'])&& !empty($_POST['senha']))
   print_r('Senha: '. $senha);*/
  $sql= "SELECT * FROM usuario WHERE email='$email' and senha= '$senha'";
 
-  $result= $conexao-> query($sql);
-  print_r($sql);
-
+ 
+ $result= $conexao-> query($sql);
+  if(mysqli_num_rows($result)<1){
+unset($SESSION['email']);
+unset($SESSION['senha']);
+header('Location: login.php');
+}else{
+  $_SESSION['email']= $_email;
+  $_SESSION['senha']= $_senha;
+  header('Location: sistema.php');
+}
 
 
 }else{
