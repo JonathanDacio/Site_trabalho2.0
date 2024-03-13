b604c6a78be2b7f63bd418a359c38f3edb785f6c
Jonathan <dacio.jhow04@gmail.com>
2024-03-13 18:15:09 -0300

    sistema login

---
+++ sistema.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <div class="navbar">
+       <a class="active" href="home">Página Incial</a>
+       <a href="#news">Minha Conta</a>
+       <a href="contact">Atendimento</a>
+       <a style="float:right" href="sair.php">Sair</a>
+    </div>
+<body>
+    
+</body>
+</html>
